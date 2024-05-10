01b359a6605a41255dfa658c55838434b0ec57cf
Kojinwoo <96171308+Kozy5@users.noreply.github.com>
2024-05-10 09:53:56 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 11.35 ms, Memory: 59.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.kt
@@ -0,0 +1,56 @@
+class Solution {
+    fun solution(s: String): Int {
+        var answer = ""
+        var eng = ""
+        for(i in s){
+            if(i !in ('0'..'9')){
+                eng+=i
+                when(eng){
+                    "zero" -> {
+                        answer += "0"
+                        eng = ""             
+                    }
+                    "one" -> {
+                        answer += "1"
+                        eng = ""             
+                    }
+                    "two" ->{
+                        answer += "2"
+                        eng = ""
+                    }
+                    "three" ->{
+                        answer += "3"
+                        eng = ""
+                    }
+                    "four" ->{
+                        answer += "4"
+                        eng = ""
+                    }
+                    "five" ->{
+                        answer += "5"
+                        eng = ""
+                    }
+                    "six" ->{
+                        answer += "6"
+                        eng = ""
+                    }
+                    "seven" ->{
+                        answer += "7"
+                        eng = ""
+                    }
+                    "eight" ->{
+                        answer += "8"
+                        eng = ""
+                    }
+                    "nine" ->{
+                        answer += "9"
+                        eng = ""
+                    }
+                }
+            }else{
+                answer+=i
+            }
+        }
+        return answer.toInt()
+    }
+}
